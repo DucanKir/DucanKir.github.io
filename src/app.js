14cd028a0eff894129955da3ce1e395775eb91b2
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-09-25 12:52:30 +0100

    home

--- src/app.js
+++ src/app.js
@@ -13,27 +13,48 @@ class App extends React.Component {
 
     render(){
       return (
-        <div className="section">
-          <div className="full-screen home">
-            <Animated animationIn="fadeInUp" animationOut="fadeOut" isVisible={true}>
-              <h1 className="title is-2 has-text-light">
-                  Lana Kirilenko
+        <div className="">
+
+          <video  className="backgroundVideo" autoPlay muted loop>
+            <source src="https://svetlana-portfolio.s3-eu-west-1.amazonaws.com/bg_video.mp4" type="video/mp4" />
+          </video>
+
+          <div className="full-screen home section">
+            <div className="">
+              <Animated animationIn="fadeInUp" animationInDuration={3000} animationOut="fadeOut" isVisible={true}>
+                <h1 className="title is-size-1 has-text-light header-text">Hello </h1>
+              </Animated>
+
+              <Animated animationIn="fadeInUp" animationInDuration={3000} animationInDelay={1000} animationOut="fadeOut" isVisible={true}>
+              <h1 className="title is-size-1 has-text-dark header-text">
+                <span className="title is-size-1 has-text-light header-text has-text-weight-bold">I'm&nbsp;</span>
+                Lana Kirilenko
               </h1>
-            </Animated>
-            <br />
-            <Animated animationIn="fadeInUp" animationOut="fadeOut" animationInDelay={1000} isVisible={true}>
-              <div className="columns">
-                <div className="column">
+              </Animated>
+
+              <Animated animationIn="fadeInRight" animationOut="fadeOut" animationInDelay={2000} animationInDuration={3000} isVisible={true}>
+                <div className="has-text-light subtitle is-3">
+                Full-stack web developer
                 </div>
-                <div className="column has-text-light">
-                    hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)hello world ;)
-                </div>
-                <div className="column">
-                </div>
+              </Animated>
+              <br />
+              <div>
+                <Animated animationIn="fadeIn" animationOut="fadeOut" animationInDelay={3000} animationInDuration={3000} isVisible={true}>
+                  <i className="fas fa-chevron-double-down" />
+                </Animated>
               </div>
-            </Animated>
+            </div>
           </div>
-          <div className="full-screen">
+
+          <div className="nav">
+            <a>Home</a>
+            <a>About</a>
+            <a>Portfolio</a>
+            <a>Contact</a>
+          </div>
+
+
+          <div className="full-screen about">
             <ScrollAnimation animateIn="flipInY" animateOnce={true}>
               <h1 className="title is-2">LANA KIR</h1>
             </ScrollAnimation>
